fd50b0429a84a30626bdca1599a347097e480a73
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-27 13:12:07 +0300

    Initial Commit

---
+++ SuperHeroBookSwiftUI/SuperHeroBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroBookSwiftUIApp.swift
+//  SuperHeroBookSwiftUI
+//
+//  Created by Tiga  on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
